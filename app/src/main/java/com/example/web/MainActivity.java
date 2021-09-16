7fc39f7eec28ae571b9d7b740b46c984735ff251
aydaraktan <aydar.aktan@mail.ru>
2021-09-16 19:23:46 +0600

    Initial commit

---
+++ app/src/main/java/com/example/web/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.web;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
